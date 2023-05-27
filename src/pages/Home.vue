14366653f576495ab0499bec03f47dc617cd3524
권혁건 <weekend12@icloud.com>
2023-05-27 21:23:41 +0900

    포트폴리오 링크 업데이트

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,8 +2,7 @@
   <div class="mt-40">
     <div class="max-w-4xl mx-auto pb-24 dark:text-white relative">
       <p class="flex items-center text-4xl font-bold">권혁건<span class="mx-5">Kwon Hyeokgeon</span></p>
-      <p class="mt-8 ml-4">✉ email : weekend12@icloud.com</p>
-      <p class="ml-4">📱 tel : 010-8338-6134</p>
+      <p class="mt-8 ml-4">✉️ email : weekend12@icloud.com</p>
       <p class="ml-4">💻 github : <a href="https://github.com/KwonHyeokGeon" target="_blank">https://github.com/KwonHyeokGeon</a></p>
       <img :src="require(`@/assets/images/face.png`)" alt="이모티콘" class="w-32 absolute md:top-5 md:right-10 right-1/2 translate-x-1/2">
     </div>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="max-w-4xl mx-auto pt-12 mt-16 pb-8">
     <Title title="포트폴리오" img="portfolio"></Title>
-    <div class="w-full mt-10 grid grid-cols-2 gap-5">
-      <div v-for="e in src" :key="e">
+    <div class="w-full mt-10 grid grid-cols-2 gap-8">
+      <div v-for="(e, index) in src" :key="e" class="relative">
         <img :src="require(`@/assets/thumbnail/${e}.png`)" :alt="e"
-          class="rounded-lg w-[430px] h-[225px] shadow-2xl dark:shadow-zinc-500 hover:scale-105 transition-all">
+          class="rounded-lg w-[430px] h-[225px] shadow-2xl dark:shadow-zinc-500 transition-all"
+          @mouseenter="modalOn(index); modal = true">
+        <div v-if="portfolio === index"
+          class="absolute top-0 left-0 flex flex-col justify-center w-full h-full px-3 bg-black/80 rounded-lg text-white transition-all"
+          :class="!modal ? 'hidden' : 'block'" @mouseleave="modal = false">
+          <p>GitHub : <a :href="github[index]" target="_blank">{{ github[index] }}</a></p>
+          <p>Link : <a :href="deploy[index]" target="_blank">{{ deploy[index] }}</a></p>
+        </div>
       </div>
     </div>
   </div>
@@ -14,11 +21,18 @@
 import Title from "../components/Title.vue";
 export default {
   name: "portfolioPage",
-  methods: {},
+  methods: {
+    modalOn(index) {
+      this.portfolio = index
+    }
+  },
   data() {
     return {
+      portfolio: null,
+      modal: false,
       src: ['broadcast', 'calculator', 'parcel', 'quiz', 'ottugi', 'youtube'],
-      href: ['https://broadcast-two.vercel.app/', 'https://calculator-five-pied.vercel.app/', 'https://parcel-ten-beta.vercel.app/', 'https://quiz-one-gilt.vercel.app/', 'https://ottugi-clone.vercel.app/', 'https://youtube-clone-navy-five.vercel.app/']
+      deploy: ['https://broadcast-two.vercel.app/', 'https://calculator-five-pied.vercel.app/', 'https://parcel-ten-beta.vercel.app/', 'https://quiz-one-gilt.vercel.app/', 'https://ottugi-clone.vercel.app/', 'https://youtube-clone-navy-five.vercel.app/'],
+      github: ['https://github.com/KwonHyeokGeon/broadcast', 'https://github.com/KwonHyeokGeon/calculator', 'https://github.com/KwonHyeokGeon/parcel', 'https://github.com/KwonHyeokGeon/quiz', 'https://github.com/KwonHyeokGeon/Ottugi-clone', 'https://github.com/KwonHyeokGeon/Youtube-clone']
     }
   },
   components: {
@@ -29,5 +43,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
