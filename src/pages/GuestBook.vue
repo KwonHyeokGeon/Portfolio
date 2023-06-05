0b82c5d066aef26443b5ab127e69943e0fecad20
KwonHyeokGeon <107496661+KwonHyeokGeon@users.noreply.github.com>
2023-06-05 16:23:10 +0900

    방명록 css 변경

--- src/pages/GuestBook.vue
+++ src/pages/GuestBook.vue
@@ -3,8 +3,8 @@
     <Title title="방명록" img="guestbook"></Title>
     <div class="w-full bg-white rounded-3xl border darkMode p-2 shadow-lg mt-10">
       <div>
-        <p v-for="e in posts" :key="e" class="flex gap-x-10 justify-around border-b px-4">
-          <span class="w-6/12">{{ e.comment }}</span><span class="w-6/12 text-right">{{ e.name }}<span class="px-4 inline-block">|</span>{{e.date}}</span>
+        <p v-for="e in posts" :key="e" class="flex gap-x-10 justify-between border-b px-4">
+          <span class="w-6/12">{{ e.comment }}</span><span class="w-4/12">{{ e.name }}<span class="px-4 inline-block">|</span>{{e.date}}</span>
         </p>
       </div>
       <div class="mt-10 flex justify-evenly darkMode">
